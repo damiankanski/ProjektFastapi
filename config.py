56b5dcd09497dd3c8ee4d427b89d0ec934f4d447
Damian Kanski <damiankanski4@gmail.com>
2023-10-16 15:11:56 +0200

    PM05- databes connection error

--- config.py
+++ config.py
@@ -1,18 +1,15 @@
 from pydantic_settings import BaseSettings
-from pydantic import Field
+
 
 
 
 class Settings(BaseSettings):
-    database_hostname: str = Field(env="DB_HOST")
-    database_port: int = Field(env="DB_PORT")
-    database_password: str = Field(env="DB_PASSWORD")
-    database_name: str = Field(env="DB_NAME")
-    database_username: str = Field(env="DB_USERNAME")
-    access_token_expire_minute: int = Field(env="ACCESS_TOKEN")
-
-    class Config:
-        env_file = ".env"
+    database_hostname: str = "localhost"
+    database_port: int = 5432
+    database_password: str = "pass"
+    database_name: str = "user"
+    database_username: str = "user"
+    access_token_expire_minute: int = 15
 
 
 settings = Settings()
